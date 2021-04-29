d7776d8b072fc06dd5f69945f26963d368e957d9
Sarpong <esarpong51@gmail.com>
2021-04-29 06:11:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const request = require('request-promise')
+const dotenv = require('dotenv')
+const dp = require('dotenv-parse')
+const {ConnectionStringParser} = require("connection-string-parser");
+const {UrlBuilder} = require('http-url-builder');
+
+module.exports = async function (connection, cb) {
+    connection = connection || process.env.CONFIG_CONNECTION_URL
+    try {
+        const {username, password, endpoint, options, hosts: [host]} = new ConnectionStringParser({scheme: 'config'})
+            .parse(connection);
+        const https = !options || !options.hasOwnProperty('ssl') || options.ssl === 'true';
+        const port = host.port || (https ? 443 : 80)
+        const url = UrlBuilder.create(host.host, port, https).addPath(username).addPath(endpoint)
+            .addPath(password).addPath('.env').build()
+        const resp = await request.get(url)
+        dp.parse(dotenv.parse(resp), {overrideProcessEnv: true})
+        cb && cb(undefined);
+    } catch (e) {
+        if (cb) return cb(e)
+        throw e;
+    }
+}
+
+
